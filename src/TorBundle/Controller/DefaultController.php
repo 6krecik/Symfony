6c7fc951322b4027754ef4deff64d7d80e0f2b55
6krecik <krecik6@gmail.com>
2015-07-22 09:51:55 +0200

    przed instalacja sonata

--- src/TorBundle/Controller/DefaultController.php
+++ src/TorBundle/Controller/DefaultController.php
@@ -10,4 +10,18 @@ class DefaultController extends Controller
     {
         return $this->render('TorBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function listUsersAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('TorBundle:User')->findAll();
+        return $this->render('TorBundle:Default:index.html.twig', array('product' => $product));
+    }
+
+    public function zmienAction()
+    {
+        echo 'zmieniam';
+        die();
+    }
+
 }
