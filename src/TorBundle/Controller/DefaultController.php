03cb716fc4dfed67f11b836c93bba42f7deab9a0
6krecik <krecik6@gmail.com>
2015-07-22 16:10:28 +0200

    zrobiony panel zarzadzania uzytkownikami wlasny

--- src/TorBundle/Controller/DefaultController.php
+++ src/TorBundle/Controller/DefaultController.php
@@ -13,15 +13,47 @@ class DefaultController extends Controller
 
     public function listUsersAction()
     {
+
         $em = $this->getDoctrine()->getManager();
         $product = $em->getRepository('TorBundle:User')->findAll();
         return $this->render('TorBundle:Default:index.html.twig', array('product' => $product));
     }
 
-    public function zmienAction()
+    public function zmienAction($id,$action, $role)
     {
-        echo 'zmieniam';
-        die();
+
+
+        $em = $this->getDoctrine()->getManager();
+        $User = $em->getRepository('TorBundle:User')->find($id);
+        if($action == 'nadaj')
+        {
+            $User->addRole($role);
+        }
+        if($action == 'odbierz')
+        {
+            $User->removeRole($role);
+        }
+        $em->persist($User);
+        $em->flush();
+        return $this->redirect($this->generateUrl('tor_users'));
+
+
+    }
+
+    public function zablokujAction($id,$action)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $User = $em->getRepository('TorBundle:User')->find($id);
+
+
+            $User->setEnabled(!$action);
+
+
+        $em->persist($User);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('tor_users'));
     }
 
 }
